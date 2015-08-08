8c819f2664a4ff8d2bfa864e8db407173ea4eb95
divitoDBA <divitobursatil@yahoo.com>
2015-08-07 22:52:24 -0300

    0.0.3

---
+++ test/test.js
@@ -0,0 +1,43 @@
+
+var expect = require('chai').expect ; 
+var grabo = require('../lib/index.js'); 
+
+describe('recLog', function(){
+    describe("recLog()", function() {
+      it("should display complete message", function(done) 
+        {
+          var args = ['paquete','funcion','mensaje'] ;
+          grabo.recLog(args[0],args[1],args[2], function(results) {
+               expect(results).to.contain('paquete');
+               expect(results).to.contain('funcion');
+               expect(results).to.contain('mensaje');
+               }) ; 
+          done() ; 
+        }) ; 
+
+      it("should display module name, unknown package and complete message", function(done) 
+        {
+          var args = ['paquete',null,'mensaje'] ;
+          grabo.recLog(args[0],args[1],args[2], function(results) {
+               expect(results).to.contain('paquete');
+               expect(results).to.contain('unknown package');
+               expect(results).to.contain('mensaje');
+               }) ; 
+          done() ; 
+        }) ; 
+
+      it("should display unknown module, unknown package and complete message", function(done) 
+        {
+          var args = [null,null,'mensaje'] ;
+          grabo.recLog(args[0],args[1],args[2], function(results) {
+               expect(results).to.contain('unknown module');
+               expect(results).to.contain('unknown package');
+               expect(results).to.contain('mensaje');
+               }) ; 
+          done() ; 
+        }) ; 
+
+
+
+     }); 
+})  ; 
